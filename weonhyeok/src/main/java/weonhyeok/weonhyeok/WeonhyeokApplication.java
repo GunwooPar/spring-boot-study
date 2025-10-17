ed64a0be45bfaf604b6da29205c742fb8c7ba998
김원혁 <kwh102455@gmail.com>
2025-10-17 17:41:07 +0900

    초기세팅 커밋

---
+++ weonhyeok/src/main/java/weonhyeok/weonhyeok/WeonhyeokApplication.java
@@ -0,0 +1,13 @@
+package weonhyeok.weonhyeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeonhyeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeonhyeokApplication.class, args);
+	}
+
+}
---
+++ weonhyeok/src/test/java/weonhyeok/weonhyeok/WeonhyeokApplicationTests.java
@@ -0,0 +1,13 @@
+package weonhyeok.weonhyeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeonhyeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
