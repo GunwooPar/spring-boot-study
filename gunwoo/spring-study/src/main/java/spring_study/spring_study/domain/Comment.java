3d92e2f6cb968e8586748346dbe58ecf9168673e
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-11-05 01:05:04 +0900

    feat: Comment 엔티티 구성요소추가
    - 댓글 상태 코드를 통한 논리적 삭제 구현
    -  댓글 수정기능 추가

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/domain/Comment.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/domain/Comment.java
@@ -9,6 +9,8 @@ import java.time.Instant;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(exclude = {"post", "user"}) // ToString은 연관관계 필드 반드시 제거해야함
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     @Id
@@ -26,4 +28,31 @@ public class Comment {
     private User user;
 
     private Instant createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        PUBLISHED,
+        DELETED
+    }
+
+
+
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void updateContent(String content, Instant createdAt) {
+        this.content = content;
+    }
+
+    public void updateCreatedAt() {
+        this.createdAt = Instant.now();
+    }
+
+    public void delete() {
+        this.status = Status.DELETED;
+    }
 }
