7156be2fd5d96b46db8cfcdb6611abcabc756b01
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-11-05 23:52:48 +0900

    feat: CommentController 구현

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/controller/web/CommentController.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/controller/web/CommentController.java
@@ -2,12 +2,14 @@ package spring_study.spring_study.controller.web;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import spring_study.spring_study.domain.Comment;
 import spring_study.spring_study.service.CommentService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,29 +19,54 @@ public class CommentController {
     private final CommentService commentService;
 
     // 댓글 조회
+    @GetMapping
+    public String comments(@PathVariable long postId, Model model) {
+        List<Comment> comments = commentService.getAllCommentsByPostId(postId);
+        model.addAttribute("comments", comments);
+        return "fragments/comment-list :: commentList";
+    }
 
     // 댓글 작성
     @PostMapping
     public String createComment(
             @PathVariable Long postId,
             @RequestParam Long userId,
-            @RequestParam String content
+            @RequestParam String content,
+            Model model
     ) {
 
-        commentService.createComment(postId, userId, content);
-        return "redirect:/board/" + postId;
+        Long commentId = commentService.createComment(postId, userId, content);
+        Comment comment = commentService.getCommentByCommentId(commentId);
+        model.addAttribute("comment", comment);
+
+        return "fragments/comment-detail :: commentDetail";
     }
 
     // 댓글 수정
+    @PutMapping("/{commentId}")
+    public String updateComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @RequestParam String content,
+            @RequestParam Long userId,
+            Model model
+    ) {
+        commentService.updateComment(postId, commentId, content);
+        Comment comment = commentService.getCommentByCommentId(commentId);
+        model.addAttribute("comment", comment);
+        return "fragments/comment-detail :: commentDetail";
+    }
 
-    // 댓글 삭제
-    @PostMapping("/{commentId}/delete")
-    public String deleteComment(
+    // 댓글 삭제는 JSON응답 (삭제시 HTML 필요없음)
+    @DeleteMapping("/{commentId}")
+    @ResponseBody   // JSON응답 시 필요
+    // 상태코드 자유자재로 제어하기위해 void 대신 ResponseEntity<Void> 반환
+    public ResponseEntity<Void> deleteComment(
             @PathVariable Long postId,
             @PathVariable Long commentId,
             @RequestParam Long userId
     ) {
         commentService.deleteComment(postId, userId, commentId);
-        return "redirect:/board/" + postId;
+        return ResponseEntity.noContent().build();
     }
 }
