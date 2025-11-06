86387e07d9202f50109db4ba28c6f20b03cf7ac7
김원혁 <kwh102455@gmail.com>
2025-11-07 00:03:37 +0900

    refactor(MemberJoinRequest): 회원가입 컨트롤러 매핑 시 사용될 DTO이다. entity password 컬럼 추가로 변경

--- weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/MemberJoinRequest.java
+++ weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/MemberJoinRequest.java
@@ -6,4 +6,7 @@ public record MemberJoinRequest(
         @NotBlank(message = "사용자이름은 필수입니다.")
         String userName,
         @NotBlank(message = "사용자 나이는 0보다 커야합니다.")
-        String userAge) {}
+        String userAge,
+        @NotBlank(message = "사용자의 비밀번호 입력은 필수입니다.")
+        String password)
+        {}
