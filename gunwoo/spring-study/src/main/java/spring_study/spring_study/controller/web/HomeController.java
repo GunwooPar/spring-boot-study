7de51320037160e88bcdc49c350bbb1a48796444
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-11-07 21:48:48 +0900

    refactor: 불필요한 어노테이션 제거
     private final 필드가 따로 없으니 생성자 주입도 안받아도되서 @requiredargsconstructor 제거

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/controller/web/HomeController.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/controller/web/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
 
     @GetMapping
