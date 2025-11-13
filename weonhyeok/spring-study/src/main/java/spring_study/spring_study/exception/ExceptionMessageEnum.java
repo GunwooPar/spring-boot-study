bb19a30844a9b54e67db45b243ecdf2ec90322f5
김원혁 <kwh102455@gmail.com>
2025-11-13 21:06:36 +0900

    feat(ExceptionMessageEnum): 예외 메시지 enum 클래스 구현

--- weonhyeok/spring-study/src/main/java/spring_study/spring_study/exception/ExceptionMessageEnum.java
+++ weonhyeok/spring-study/src/main/java/spring_study/spring_study/exception/ExceptionMessageEnum.java
@@ -1,4 +1,15 @@
 package spring_study.spring_study.exception;
 
 public enum ExceptionMessageEnum {
+    NO_DATA_EXCEPTION("질문글을 찾을 수 없거나 질문글이 존재하지 않습니다.");
+
+    private final String errorMessage;
+
+    ExceptionMessageEnum(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage(){
+        return errorMessage;
+    }
 }
