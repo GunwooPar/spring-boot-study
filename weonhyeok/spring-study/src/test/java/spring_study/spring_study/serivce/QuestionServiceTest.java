3c94337b17019ea3a9493ca1815b4162f5d4355f
김원혁 <kwh102455@gmail.com>
2025-11-14 15:39:17 +0900

    feat(AnswerService): 답변 서비스 구현중

---
+++ weonhyeok/spring-study/src/test/java/spring_study/spring_study/serivce/QuestionServiceTest.java
@@ -0,0 +1,31 @@
+package spring_study.spring_study.serivce;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import spring_study.spring_study.dto.QuestionResponse;
+import spring_study.spring_study.repository.QuestionRepository;
+import spring_study.spring_study.service.QuestionService;
+
+@SpringBootTest
+@Transactional
+public class QuestionServiceTest {
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private QuestionService questionService;
+
+    @DisplayName("없는 질문 검색으로 예외 발생시키기")
+    @Test
+    void 질문상세페이지가져오기_테스트(){
+        //given
+        final Long id = 999L;
+
+        //when
+        QuestionResponse question = questionService.getQuestion(id);
+
+        //then
+    }
+}
