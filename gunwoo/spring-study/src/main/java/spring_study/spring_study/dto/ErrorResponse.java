c4e93382cde7a9e517f3bd039ed7f1a212ad2e59
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-11-17 13:44:32 +0900

    refactor: null 안전성 검사 위한 @NonNull 추가

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/dto/ErrorResponse.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/dto/ErrorResponse.java
@@ -1,16 +1,17 @@
 package spring_study.spring_study.dto;
 
-
 import spring_study.spring_study.exception.ErrorCode;
+import lombok.NonNull;
 
 public record ErrorResponse (
     int httpStatus,
     String message
 ) {
-    public static ErrorResponse of(ErrorCode errorCode) {
+    public static ErrorResponse of(@NonNull ErrorCode errorCode) {
         return new ErrorResponse(
                 errorCode.getHttpStatus().value(),
                 errorCode.getMessage()
+
         );
     }
 }
