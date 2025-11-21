45f89f551b4f2bee0bc3f5783f685f35f148a927
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-11-21 21:53:21 +0900

    fix: InvalidInputValueException에서 사용하지않는 변수 제거

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/exception/InvalidInputValueException.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/exception/InvalidInputValueException.java
@@ -2,7 +2,7 @@ package spring_study.spring_study.exception;
 
 public class InvalidInputValueException extends CustomException {
 
-    public InvalidInputValueException(String message) {
+    public InvalidInputValueException() {
         super(ErrorCode.INVALID_INPUT_VALUE);
     }
 }
