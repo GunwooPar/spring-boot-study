7b2940d64df3cdb00ef0e10a04297a424e91610e
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-12-03 19:56:46 +0900

    refactor: UserRepository 수정

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/repository/UserRepository.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/repository/UserRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import spring_study.spring_study.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByUsername(String username);
+    // List<User> → Optional<User> 변경 (username은 unique하므로)
+    Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username); // 중복체크
 
