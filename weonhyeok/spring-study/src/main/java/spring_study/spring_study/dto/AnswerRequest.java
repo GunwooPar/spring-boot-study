884df905a6617be1b5828fbac1e5374a51b6df84
김원혁 <kwh102455@gmail.com>
2025-12-05 17:25:11 +0900

    feat: @NotBlank message 인수값 보냄

--- weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/AnswerRequest.java
+++ weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/AnswerRequest.java
@@ -1,13 +1,15 @@
 package spring_study.spring_study.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
 public record AnswerRequest(
     Long id,
-    @NotBlank
+    @NotBlank(message = "답변 제목은 필수입니다.")
+    @Size(max = 200)
     String content,
-    @NotBlank
+    @NotBlank(message = "답변 내용은 필수입니다.")
     LocalDateTime createDate
 ) {}
