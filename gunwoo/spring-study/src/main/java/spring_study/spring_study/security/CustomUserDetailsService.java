0737b122627d4c9eaa7652874e03644d70ac5895
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-12-07 14:40:49 +0900

    fix: CustomUserDetailsService 복구

---
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package spring_study.spring_study.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import spring_study.spring_study.domain.User;
+import spring_study.spring_study.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: "+ username));
+
+        return new CustomUserDetails(user);
+    }
+}
