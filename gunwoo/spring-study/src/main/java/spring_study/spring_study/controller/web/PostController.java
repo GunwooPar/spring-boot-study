2ee76306e6eb4772018d67fdc2056d3b48837271
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-12-07 16:53:39 +0900

    refactor: userId 하드 코딩 방식에서 개선

--- gunwoo/spring-study/src/main/java/spring_study/spring_study/controller/web/PostController.java
+++ gunwoo/spring-study/src/main/java/spring_study/spring_study/controller/web/PostController.java
@@ -37,12 +37,16 @@ public class PostController {
 
     // 게시글 상세(댓글 목록도 함께 조회)
     @GetMapping("/{id}")
-    public String boardDetail(@PathVariable Long id, Model model) {
+    public String boardDetail(@PathVariable Long id,
+                              Model model,
+                              @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         Post post = postService.getPost(id);
         List<Comment> comments = commentService.getAllCommentsByPostId(id);
 
         model.addAttribute("post", post);
         model.addAttribute("comments", comments);
+        model.addAttribute("userDetails", userDetails);
         return "board-detail";
     }
 
