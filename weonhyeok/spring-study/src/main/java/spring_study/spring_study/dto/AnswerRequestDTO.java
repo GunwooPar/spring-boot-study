5456aa780e082fd1ec57c9f8250cc597d3d77bef
김원혁 <kwh102455@gmail.com>
2025-12-12 13:55:20 +0900

    refactor: 네이밍 변경

--- weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/AnswerRequestDTO.java
+++ weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/AnswerRequestDTO.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
  * @param content 답변 제목
  * @param createDate 답변 생성 날짜
  */
-public record AnswerRequest(
+public record AnswerRequestDTO(
     @NotBlank(message = "답변 제목은 필수입니다.")
     @Size(max = 500)
     String content,
--- weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/AnswerResponseDTO.java
+++ weonhyeok/spring-study/src/main/java/spring_study/spring_study/dto/AnswerResponseDTO.java
@@ -4,13 +4,13 @@ import spring_study.spring_study.domain.Answer;
 
 import java.time.LocalDateTime;
 
-public record AnswerResponse (
+public record AnswerResponseDTO(
         Long id,
         String content,
         LocalDateTime createDate
 ) {
-    public static AnswerResponse from(Answer answer) {
-        return new AnswerResponse(
+    public static AnswerResponseDTO from(Answer answer) {
+        return new AnswerResponseDTO(
                 answer.getId(),
                 answer.getContent(),
                 answer.getCreateDate()
